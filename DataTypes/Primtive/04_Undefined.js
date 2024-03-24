7d3e71d1907d1f0eca751c47d56619c425675c9f
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:45:12 +0530

    updated

---
+++ DataTypes/Primtive/04_Undefined.js
@@ -0,0 +1,7 @@
+let age;
+console.log(age);   // Output: undefined
+console.log(typeof age);    // Output: undefined
+
+age = 25;
+console.log(age);   // Output: 25
+console.log(typeof age);    // Output: number
