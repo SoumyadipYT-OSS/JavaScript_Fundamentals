9d677f4dd2a58e7e7cb5b6f5941682b45af0ee91
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 13:01:00 +0530

    updated

---
+++ Operators/01_Arithmetic.js
@@ -0,0 +1,56 @@
+// Addition
+let sum = 5 + 3;
+console.log(sum); // Output: 8
+
+// Subtraction
+let difference = 5 - 3;
+console.log(difference); // Output: 2
+
+// Multiplication
+let product = 5 * 3;
+console.log(product); // Output: 15
+
+// Division
+let quotient = 5 / 3;
+console.log(quotient); // Output: 1.6666666666666667
+
+// Exponentiation
+let square = 5 ** 2;
+console.log(square); // Output: 25
+
+// Modulus
+let remainder = 5 % 3;
+console.log(remainder); // Output: 2
+
+// Increment
+let num = 5;
+num++;
+console.log(num); // Output: 6
+
+// Decrement
+num--;
+console.log(num); // Output: 5
+
+// Pre-Increment
+let a = 5;
+let b = ++a;
+console.log(a); // Output: 6
+console.log(b); // Output: 6
+
+// Post-Increment
+let x = 5;
+let y = x++;
+console.log(x); // Output: 6
+console.log(y); // Output: 5
+
+
+// Pre-Decrement
+let c = 5;
+let d = --c;
+console.log(c); // Output: 4
+
+
+// Post-Decrement
+let p = 5;
+let q = p--;
+console.log(p); // Output: 4
