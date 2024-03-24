e6b1901580636a7ea0085026da5fc738d2b2a054
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 16:05:12 +0530

    code updated

--- Control_Flow_Statement/Conditional statements/01_conditional.js
+++ Control_Flow_Statement/Conditional statements/01_conditional.js
@@ -12,7 +12,7 @@ if (age >= 18) {
 let country = "India";
 let ageCriteria = 18;
 if (ageCriteria >= 18 && country == "India") {
-    console.log('You can vote');
+    console.log('You can get driving licenses');
 } else {
-    console.log('You cannot vote');
+    console.log('You cannot get driving licenses');
 }
