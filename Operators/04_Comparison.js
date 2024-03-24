3da6fe87e2d01160f8bb03836fc18aebc803518a
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 14:35:33 +0530

    updated

---
+++ Operators/04_Comparison.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+let x = 5;
+let y = 3;
+
+console.log(x == y); // Output: false
+console.log(x != y); // Output: true
+console.log(x > y); // Output: true
+console.log(x < y); // Output: false
+console.log(x >= y);
+console.log(x <= y);
+console.log(x === y);   // strict equality operator
