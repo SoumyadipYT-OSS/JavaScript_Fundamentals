1f550d0f1999a9fe056b844f37e418841a368940
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 14:32:13 +0530

    updated

---
+++ Operators/03_IncrementDecrement.js
@@ -0,0 +1,20 @@
+/* Increment and Decrement operator in JavaScript */
+// pre-increment
+let a = 10;
+console.log(++a); // Output: 11
+console.log(a); // Output: 11
+
+// post-increment
+let b = 20;
+console.log(b++); // Output: 20
+console.log(b); // Output: 21
+
+// pre-decrement
+let c = 30;
+console.log(--c); // Output: 29
+console.log(c); // Output: 29
+
+// post-decrement
+let d = 40;
+console.log(d--); // Output: 40
+console.log(d); // Output: 39
