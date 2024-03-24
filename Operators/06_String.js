b7747a73edc8c3bb7ec7e4e585b8f0bd192f2835
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 15:14:06 +0530

    updated

---
+++ Operators/06_String.js
@@ -0,0 +1,6 @@
+// String operator
+// The + operator can also be used to concatenate (join) two string values together, as shown below:
+
+let a = "JavaScript";
+a += " is fun";
+console.log(a); // Output: JavaScript is fun
