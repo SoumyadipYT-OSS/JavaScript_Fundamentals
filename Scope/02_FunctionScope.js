8d228593b7c2bf07d5f6992bf4a49e7de0e1ef44
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:22:10 +0530

    updated

---
+++ Scope/02_FunctionScope.js
@@ -0,0 +1,14 @@
+// Function Scope
+/*
+    Function scope is created when a function is declared.
+    Variables and functions declared in a function's scope are
+    only visible within the function.
+ */
+
+function example() {
+    var fs = "Function Scope";
+
+    console.log(fs);
+}
+
+example();
