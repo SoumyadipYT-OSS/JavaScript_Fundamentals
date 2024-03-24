23efe16babcbe3b9152984599deffca2cdca874d
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:18:34 +0530

    updated

---
+++ Scope/01_Scope.js
@@ -0,0 +1,12 @@
+// Global Scope
+/*
+    The global scope is the outermost scope in a JavaScript program. Variables and functions declared
+    in the global scope are visible from anywhere in the program.
+ */
+
+var x = "JavaScript Language";
+
+function example() {
+console.log(x);
+}
+example();
