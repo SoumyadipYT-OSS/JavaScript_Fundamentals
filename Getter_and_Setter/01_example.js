2b0948a3d298bc9dbec004bab65f9772a954196a
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-25 01:19:45 +0530

    updated

---
+++ Getter_and_Setter/01_example.js
@@ -0,0 +1,32 @@
+class Bicycle {
+    constructor(brand, model) {
+        this._brand = brand;
+        this._model = model;
+    }
+
+    get brand() {
+        return this._brand;
+    }
+
+    set brand(newBrand) {
+        this._brand = newBrand;
+    }
+
+    get model() {
+        return this._model;
+    }
+
+    set model(newModel) {
+        this._model = newModel;
+    }
+}
+
+
+let myBike = new Bicycle('Trek', 'Domane');
+console.log(myBike.brand); // Outputs: Trek
+console.log(myBike.model); // Outputs: Domane
+
+myBike.brand = 'Specialized';
+myBike.model = 'Roubaix';
+console.log(myBike.brand); // Outputs: Specialized
+console.log(myBike.model); // Outputs: Roubaix
