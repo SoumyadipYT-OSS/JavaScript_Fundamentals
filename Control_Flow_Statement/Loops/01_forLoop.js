07b98cb17322d1f1105c30c4cb1adc7d3edea0d7
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 17:00:00 +0530

    updated

---
+++ Control_Flow_Statement/Loops/01_forLoop.js
@@ -0,0 +1,16 @@
+// for loop
+
+// for loop is used to execute a block of code multiple times.
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+// for loop with array
+let coding = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+
+for (let i = 0; i < coding.length; i++) {
+    console.log(coding[i]);
+}
