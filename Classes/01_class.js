92b6757ddeb40cc3d094aaf9a145550fb2618995
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-25 00:27:03 +0530

    updated

--- Classes/01_class.js
+++ Classes/01_class.js
@@ -8,9 +8,19 @@ class Person {
     greet() {
         return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
     }
+    favouriteFood(food) {
+        this.food = food;
+    }
 }
 
 const person1 = new Person("Soumyadip Majumder", 20);
 const person2 = new Person("Bill Gates", 68);
+console.log(person1);
+console.log(person2);
+console.log(person1.greet());
+console.log(person2.greet());
+
+person1.favouriteFood("Paneer Butter Masala");
+person2.favouriteFood("Pizza and Chai");
 console.log(person1);
 console.log(person2);
