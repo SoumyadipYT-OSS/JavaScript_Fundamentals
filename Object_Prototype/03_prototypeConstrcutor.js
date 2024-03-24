fcca297a2f9cfd81b9bc09d32ce5894c6143f14e
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 22:17:58 +0530

    updated

---
+++ Object_Prototype/03_prototypeConstrcutor.js
@@ -0,0 +1,15 @@
+function Person() {
+    this.name = "Soumyadip Majumder";
+}
+Person.prototype.age = 25;
+
+const person1 = new Person();
+
+Person.prototype = { age: 30 };
+
+const person2 = new Person();
+
+console.log(person1);
+console.log(person1.age);
+console.log(person2);
+console.log(person2.age);
