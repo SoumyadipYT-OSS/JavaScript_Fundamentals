ce7ded509ab4cf04273c0b620edd73d681804975
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 16:04:26 +0530

    updated

---
+++ Control_Flow_Statement/Conditional statements/01_conditional.js
@@ -0,0 +1,18 @@
+// Conditional Statements
+
+let age = 16;
+if (age >= 18) {
+    console.log('You are an adult');
+} else {
+    console.log('You are not an adult.');
+}
+
+
+
+let country = "India";
+let ageCriteria = 18;
+if (ageCriteria >= 18 && country == "India") {
+    console.log('You can vote');
+} else {
+    console.log('You cannot vote');
+}
