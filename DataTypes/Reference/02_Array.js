a2c3c5d26de83bfc565e0204bf6e850dbf2dbc80
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:50:54 +0530

    updated

---
+++ DataTypes/Reference/02_Array.js
@@ -0,0 +1,6 @@
+// In JavaScript Arrays reference datatype is used to store multiple values in a single variable.
+
+let number = [1,2,3,4,5];
+
+console.log(number);    // Output: [1, 2, 3, 4, 5]
+console.log(typeof number); // Output: object
