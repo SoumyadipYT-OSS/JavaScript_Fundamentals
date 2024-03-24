828eebc22477c1c035460cc795ad69cc52503061
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 19:00:11 +0530

    code updated

---
+++ CallBack/03_SetTimeoutAnonymous.js
@@ -0,0 +1,8 @@
+// SetTimeout is a function that is used to execute a function after a specified amount of time.
+// It is a built-in function in JavaScript.
+
+setTimeout(
+    function () {
+        console.log("JavaScript Programming");
+    }
+,3000); // 3000 milliseconds = 3 seconds; after 3 seconds, the function will be executed
