cb254838bc8d63e996428c1bd98f90e49e5047fa
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:37:05 +0530

    updated

---
+++ DataTypes/Primtive/02_Numbers.js
@@ -0,0 +1,12 @@
+let num = 100;
+let num1 = 96.9;
+let num2 = 96.0;
+console.log(num);
+console.log(num1);
+console.log(num2);
+
+
+let x = 10;
+console.log(typeof x);
+x = '10';
+console.log(typeof x);
