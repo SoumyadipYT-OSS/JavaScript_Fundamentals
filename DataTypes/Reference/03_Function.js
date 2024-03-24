c4425980d8ab442e9525ef7d612ddb77ea884204
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 12:52:07 +0530

    updated

---
+++ DataTypes/Reference/03_Function.js
@@ -0,0 +1,8 @@
+// In JavaScript Function datatype is used to store a block of code that can be executed whenever required.
+
+function msg() {
+    console.log("JavaScript programming");
+}
+
+console.log(typeof msg);    // Output: function
+log(msg);    // Output: JavaScript programming
