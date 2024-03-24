bee3bba5935b83d0dcb13d9777c68e411f6e13d5
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 13:11:05 +0530

    updated

---
+++ Operators/02_Assignment.js
@@ -0,0 +1,25 @@
+// Assignment operators
+
+let x = 5;
+x += 3;
+console.log(x); // Output: 8
+
+let y = 5;
+y -= 3;
+console.log(y); // Output: 2
+
+let z = 5;
+z *= 3;
+console.log(z); // Output: 15
+
+let a = 5;
+a /= 3;
+console.log(a); // Output: 1.6666666666666667
+
+let b = 5;
+b %= 3;
+console.log(b); // Output: 2
+
+let c = 5;
+c **= 3;
+console.log(c); // Output: 125
