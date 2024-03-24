47c4b469599f6a835b9370f1b430b6b1a6f0b763
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 18:28:52 +0530

    updated

---
+++ Function/02_Function_withParameters.js
@@ -0,0 +1,10 @@
+function sum(x, y) {
+    return x + y;
+}
+console.log(sum(5, 5)); // Output: 10
+
+
+function Name(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+console.log(Name("Soumyadip", "Majumder"));
