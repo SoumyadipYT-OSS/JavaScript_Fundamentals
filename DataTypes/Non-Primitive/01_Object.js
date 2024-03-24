b4895f9f82deba99d9e5868f01abe76af63346d8
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 19:45:08 +0530

    updated

---
+++ DataTypes/Non-Primitive/01_Object.js
@@ -0,0 +1,16 @@
+const object_name = {
+    foodName: "Banana Shake",
+    fruit: "Banana",
+    Liquid: "Milk",
+    nut: "Keshar and Kaju",
+    price: 100,
+}
+
+console.log(object_name);
+console.log(object_name.foodName);
+
+
+object_name.foodName = "Mango Shake";
+object_name.fruit = "Mango";
+console.log(object_name);
+console.log(object_name.foodName);
