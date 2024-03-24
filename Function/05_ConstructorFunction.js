1bff3c45eb7b4be2eeebf70f88156fa2548ee3dc
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-24 21:22:59 +0530

    updated

--- Function/05_ConstructorFunction.js
+++ Function/05_ConstructorFunction.js
@@ -11,7 +11,18 @@ function Person() {
 }
 
 const person1 = new Person();
-const person2 = new Person();
+const person2 = new Person("Bill", "Gates");    // This will not work as we have not passed any parameter in the constructor function
 
 console.log(person1);
 console.log(person2);
+
+person1.age = 30;   // Adding new property to the object
+person2.age = 68;   // Adding new property to the object
+console.log(person1);
+console.log(person2);
+
+person2.greet = function() {
+    console.log("Hello, " + person1.firstName + " " + person1.lastName);
+}
+console.log(person2);
+person2.greet();    // This will not work as greet function is not defined in the constructor function
