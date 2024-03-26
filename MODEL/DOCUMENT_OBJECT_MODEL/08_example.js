67964bfdabfcd1054d45b78a1a382522d8d86b5c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-26 22:44:26 +0530

    updated

--- MODEL/DOCUMENT_OBJECT_MODEL/08_example.js
+++ MODEL/DOCUMENT_OBJECT_MODEL/08_example.js
@@ -1,4 +1,4 @@
 let menu = document.getElementById('menu');
-menu.insertAdjacentHTML('beforeend', '<li>browse</li>');
-menu.insertAdjacentHTML('beforeend', '<li>search</li>');
-parent.replaceChild(newChild, oldChild);
+
+let newNode = menu.cloneNode();
+console.log(newNode);
