f27cd47f09ed0339342ecd9a20c56cd7bf06a7eb
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-26 16:28:13 +0530

    code updated

---
+++ MODEL/DOCUMENT_OBJECT_MODEL/03_example.js
@@ -0,0 +1,4 @@
+// parentNode property:
+// The parentNode property returns the parent node of the specified node, as a Node object.
+let txt = document.querySelector(".text");
+console.log(txt.parentNode);
