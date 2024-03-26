56ecb844b01fbd81f2852c245c986255b0bcc206
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-26 20:15:24 +0530

    code updated

--- MODEL/DOCUMENT_OBJECT_MODEL/04_example.js
+++ MODEL/DOCUMENT_OBJECT_MODEL/04_example.js
@@ -10,3 +10,9 @@ console.log(parent.childNodes);
 // previousSibling is a property of the Node interface. It returns the previous sibling of the specified node, including text nodes. If the node has no previous sibling, it returns null.
 let second = document.querySelector(".second")
 console.log(second.previousSibling);
+let second1 = document.querySelector(".second")
+console.log(second1.previousElementSibling);
+let secondNext = document.querySelector(".second")
+console.log(secondNext.nextSibling);
+let secondNext1 = document.querySelector(".second")
+console.log(secondNext1.nextElementSibling);
