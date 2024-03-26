bd2bdf8c2958912115cf2cf6966a73d95b2dc41d
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-26 17:18:00 +0530

    updated

---
+++ MODEL/DOCUMENT_OBJECT_MODEL/04_example.js
@@ -0,0 +1,6 @@
+let parent = document.querySelector(".title");
+
+console.log(parent.firstChild);
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
+console.log(parent.childNodes);
