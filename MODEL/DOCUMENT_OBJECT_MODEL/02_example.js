ae337076506c8cb7d9b32392eb2a9b71712da9c0
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-26 16:22:32 +0530

    updated

--- MODEL/DOCUMENT_OBJECT_MODEL/02_example.js
+++ MODEL/DOCUMENT_OBJECT_MODEL/02_example.js
@@ -15,6 +15,6 @@ let divPart = document.querySelectorAll("div");
 console.log(divPart);
 
 
-let cont = document.getElementById("container");
+let cont = document.getElementById("#container");
 let msg1 = cont.querySelectorAll(".message");
 console.log(msg1);
