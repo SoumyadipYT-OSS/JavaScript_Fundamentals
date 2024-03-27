d910846d913b6b4e30b16a78fcb15e6c39b47cb8
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-28 00:14:31 +0530

    updated

--- MODEL/EVENT/01_ex.js
+++ MODEL/EVENT/01_ex.js
@@ -3,4 +3,14 @@ function displayMsg() {
 }
 
 let btn = document.getElementById("btn");
-btn.onclick = null;
+function displayMsg1() {
+    console.log("Button1 clicked From function!")
+}
+
+
+
+let btn1 = document.getElementById("btn1");
+// by default, the value of useCapture is false
+btn1.addEventListener('click', function(event){
+    console.log("Button using Event Listener clicked!" + event.type);
+})
