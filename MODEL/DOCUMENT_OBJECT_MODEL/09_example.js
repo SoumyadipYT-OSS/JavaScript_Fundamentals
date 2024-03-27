f5ed31a43ff973ced54e69a6ae9ea64a786a44c4
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-27 18:56:37 +0530

    updated

---
+++ MODEL/DOCUMENT_OBJECT_MODEL/09_example.js
@@ -0,0 +1,6 @@
+let inputBox = document.getElementById("username")
+
+// hasAttribute() method
+// The hasAttribute() method checks whether an element has a particular attribute or not.
+console.log(inputBox.hasAttribute("id"));
+console.log(inputBox.hasAttribute("class"));
